05d3995d16e635620d1bbac805ec3cdb2c00a05a
AlbeTorres <albertocorreoficial@gmail.com>
2024-03-24 19:27:45 -0400

    feat: add header component

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,5 +1,14 @@
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>
+import Header from '@/components/shared/Header'
+import { transformationTypes } from '@/constants'
+
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const { title, subTitle } = transformationTypes[type]
+
+  return (
+    <>
+      <Header title={title} subtitle={subTitle} />
+    </>
+  )
 }
 
 export default AddTransformationTypePage
